44ac6daea471f82c7d405b35ef8562d72d43f0c2
Sunny <ring2rings93@gmail.com>
2019-06-26 20:37:20 +0100

    added game board, render play, accept clicks from board

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,42 @@
-alert("working!")
+document.addEventListener('DOMContentLoaded',()=>{
+
+GameBoard.render()
+})
+
+
+const GameBoard = (() =>{
+  positions = ['x','o','x','-','x','-','o','-','o']
+
+  function render (){
+    positions.forEach((piece, index)=>{
+      document.getElementById(`cell-${index}`).innerHTML = piece
+    })
+  }
+
+  function play(index){
+    console.log(` cell ${index} was taped`)
+  }
+  return{
+    positions, render, play
+  }
+})()
+
+
+const GamePlay = (()=>{
+
+  return{
+
+  }
+})()
+
+const player = (name, piece)=>{
+  
+  function play(piece){
+    // pick position on board
+  }
+
+  return{
+    name, piece, play
+  }
+
+}
