27656965780ce71d86c5355b2cff6bf4f103f2f2
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-07-02 20:42:30 -0500

    [add] refactor to work with dependency injection

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,102 @@
+const DOMHelpers = (() => {
+  let $message = document.getElementById('game-message')
 
-const GameBoard = (() => {
-  positions = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
+  function render(positions) {
+    positions.forEach((piece, index) => {
+      document.getElementById(`cell-${index}`).innerHTML = piece
+    })
+  }
+
+  function setMessage(msg) {
+    $message.innerHTML = msg
+  }
+
+  return { render, setMessage }
+})()
+
+const GamePlay = (() => {
+  let player1, player2
+  let currentPlayer
+  let isPlaying = false
+  let board, showMessage
+
+  function init({ board, showMessage}) {
+    setBoard(board)
+    setShowMessage(showMessage)
+    const { playerOneName, playerTwoName } = getPlayersNames()
+    player1 = Player(playerOneName, 'O')
+    player2 = Player(playerTwoName, 'X')
+    currentPlayer = player1
+    showMessage(`${playerOneName}'s Turn`)
+    isPlaying = true
+  }
+
+  function setBoard (boardInstance) {
+    board = boardInstance
+  }
+
+  function setShowMessage (messageFunction) {
+    showMessage = messageFunction
+  }
+
+  function getPlayersNames() {
+    let playerOneName, playerTwoName
+
+    while (playerOneName == null || playerOneName == "") {
+      playerOneName = prompt("Enter player 1's name ", 'Player 1')
+    }
+    while (playerTwoName == null || playerTwoName == "") {
+      playerTwoName = prompt("Enter player 2's name ", 'Player 2')
+    }
+
+    return { playerOneName, playerTwoName }
+  }
+
+  function play(index) {
+    if (board.isPositionOcupied(index) || !isPlaying) return
+    currentPlayer.makeMove(index)
+    board.markPosition(index, currentPlayer.piece)
+    const { win, tie } = checkGame(currentPlayer.piece)
+    if (win || tie) {
+      const msg = win ? `${currentPlayer.name} Won!!!` : `It's a Tie`
+      showMessage(msg)
+      isPlaying = false
+    } else {
+      togglePlayer()
+    }
+  }
+
+  function checkGame () {
+    const win = board.movesInWinningMoves(currentPlayer.moves)
+    const tie = board.isFull()
+    return { win, tie }
+  }
+
+
+
+  function togglePlayer() {
+    currentPlayer = currentPlayer == player1 ? player2 : player1
+    showMessage(`${currentPlayer.name}'s turn`)
+  }
+
+  return { init, currentPlayer, togglePlayer, play }
+})()
+
+const Player = (name, piece) => {
+  let moves = []
+
+  function makeMove(index){
+    moves.push(index)
+  }
+
+  return{
+    name, piece, makeMove, moves
+  }
+}
+
+const makeBoard = (render) => {
+
+  let positions = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
 
   let winningMoves = [
     [0, 1, 2],
@@ -13,20 +109,9 @@ const GameBoard = (() => {
     [2, 4, 6]
   ]
 
-  function render() {
-    positions.forEach((piece, index) => {
-      document.getElementById(`cell-${index}`).innerHTML = piece
-    })
-  }
-
-  function reset() {
-    positions = positions.map(pos => '-')
-    render()
-  }
-
   function markPosition(index, piece) {
     positions[index] = piece
-    render()
+    render(positions)
   }
 
   function isFull(){
@@ -44,85 +129,19 @@ const GameBoard = (() => {
 
   const isPositionOcupied = index => positions[index] != '-'
 
-  return { reset, markPosition,
+  render(positions)
+
+  return { render, markPosition,
     isPositionOcupied, getPositions,
     isFull, movesInWinningMoves
   }
-})()
-
-
-const GamePlay = (() => {
-  let player1, player2
-  let currentPlayer
-  let $message = document.getElementById('game-message')
-  let isPlaying = false
-
-  function init() {
-    GameBoard.reset()
-    const { playerOneName, playerTwoName } = getPlayersNames()
-    player1 = Player(playerOneName, 'O')
-    player2 = Player(playerTwoName, 'X')
-    currentPlayer = player1
-    setMessage(`${playerOneName}'s Turn`)
-    isPlaying = true
-  }
-
-  function getPlayersNames() {
-    let playerOneName, playerTwoName
-
-    while (playerOneName == null || playerOneName == "") {
-      playerOneName = prompt("Enter player 1's name ", 'Player 1')
-    }
-    while (playerTwoName == null || playerTwoName == "") {
-      playerTwoName = prompt("Enter player 2's name ", 'Player 2')
-    }
-
-    return { playerOneName, playerTwoName }
-  }
-
-  function play(index) {
-    if (GameBoard.isPositionOcupied(index) || !isPlaying) return
-    currentPlayer.makeMove(index)
-    const { win, tie } = checkGame(currentPlayer.piece)
-    if (win || tie) {
-      const msg = win ? `${currentPlayer.name} Won!!!` : `It's a Tie`
-      setMessage(msg)
-      isPlaying = false
-    } else {
-      togglePlayer()
-    }
-  }
-
-  function checkGame () {
-    const win = GameBoard.movesInWinningMoves(currentPlayer.moves)
-    const tie = GameBoard.isFull()
-    return { win, tie }
-  }
-
-  function setMessage(msg) {
-    $message.innerHTML = msg
-  }
-
-  function togglePlayer() {
-    currentPlayer = currentPlayer == player1 ? player2 : player1
-    setMessage(`${currentPlayer.name}'s turn`)
-  }
-
-  return { init, currentPlayer, togglePlayer, play }
-})()
-
-const Player = (name, piece) => {
-  let moves = []
-
-  function makeMove(index){
-    moves.push(index)
-    GameBoard.markPosition(index, piece)
-  }
-
-  return{
-    name, piece, makeMove, moves
-  }
 }
 
+function startGame () {
+  GamePlay.init({
+    board: makeBoard(DOMHelpers.render),
+    showMessage: DOMHelpers.setMessage
+  })
+}
 
-GamePlay.init()
+startGame()
